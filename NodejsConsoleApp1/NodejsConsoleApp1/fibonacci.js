d1ebc8df015cc3a8c9c1879b5f5ca4dc85a3c881
baiyalong <baiyalong@live.cn>
2015-11-16 20:38:42 +0800

    fb

---
+++ NodejsConsoleApp1/NodejsConsoleApp1/fibonacci.js
@@ -0,0 +1,25 @@
+﻿//fibonacci
+//n
+
+'use strict'
+const N = 100;
+
+function fibonacci(n) {
+	if (n == 1 || n == 2)
+		return 1;
+	
+	let p=1,q=1,r=0;
+	for (let i=3; i <= n; i++) {
+		r = p + q;
+		p = q;
+		q = r;
+		console.log(r);
+	}
+	return r;
+}
+
+var res = fibonacci(N);
+console.log(res);
+
+
+exports.fibonacci = fibonacci;
