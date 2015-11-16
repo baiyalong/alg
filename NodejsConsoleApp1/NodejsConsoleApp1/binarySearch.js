10078337900fd6580b648e5a9841dfb87498a9e2
baiyalong <baiyalong@live.cn>
2015-11-16 20:12:22 +0800

    bs

--- NodejsConsoleApp1/NodejsConsoleApp1/binarySearch.js
+++ NodejsConsoleApp1/NodejsConsoleApp1/binarySearch.js
@@ -11,14 +11,22 @@ X = arr[N];
 
 console.log(arr, N, X);
 
-function binarySearch(a, x) {
+function binarySearch(a, l, r, x) {
 	
+	if (r - l == 1 && x == a[l])
+		return l;
 	
-	const m = Math.round((a.length - 1) / 2);
-
+	const m = Math.round((r + l) / 2);
+	let res = 0;
+	if (a[m] > x)
+		res = binarySearch(a, l, m, x);
+	else
+		res = binarySearch(a, m, r, x);
+	
+	return res;
 }
 
-var index = binarySearch(arr, X);
+var index = binarySearch(arr, 0, arr.length, X);
 
 console.log(arr, index, arr[index])
 
